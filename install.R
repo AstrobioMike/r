4e7d54d33fa1bfb9a947eb7a4ffb09f4d5398b1f
AstrobioMike <michael.lee0517@gmail.com>
2020-04-01 20:40:46 -0700

    adding full install.R file

--- install.R
+++ install.R
@@ -1,1 +1,20 @@
 install.packages("vegan")
+install.packages("dendextend")
+install.packages("tidyr")
+install.packages("viridis")
+install.packages("reshape")
+
+install.packages("BiocManager")
+
+BiocManager::install("phyloseq")
+BiocManager::install("DESeq2")
+BiocManager::install("ShortRead", suppressUpdates = FALSE)
+
+install.packages("devtools")
+devtools::install_github("benjjneb/dada2")
+devtools::install_github("benjjneb/decontam")
+
+devtools::install_version("mvtnorm", version = "1.0-8", repos = "http://cran.us.r-project.org")
+devtools::install_version("fpc", version = "2.1-11.1", repos = "http://cran.us.r-project.org")
+
+BiocManager::install("dendextend")
